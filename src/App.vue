36623f987fa07f6dddfc229b772995deb79f6219
pristtit <pristtit@mail.ru>
2022-10-02 22:45:55 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="a"></div>
+</template>
+
+<style scoped lang="scss">
+.a {
+    width: 20px;
+    height: 20px;
+    background-color: black;
+}
+</style>
